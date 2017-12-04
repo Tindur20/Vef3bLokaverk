e2697865d6fa4dc55488458869452035e6fab82a
Tindur20 <Sigsteinn@gmail.com>
2017-12-04 00:49:19 +0000

    Lagaði smá hvenring kóðinn lítur og bæti inn tvemur function(virkar ekki)

--- scripts.js
+++ scripts.js
@@ -1,23 +1,29 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+var width = window.innerWidth,
+	height = window.innerHeight,
+	centered;
+
+var projection = d3.geoAlbers()
+    .center([-1.0, 65])
+    .rotate([18, 0])
+    .parallels([50, 60])
+    .scale(7000)
+    .translate([width / 2, height / 2]);    
+
+var path = d3.geoPath()
+    .projection(projection);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("fill", "#31ff2f")
     .attr("stroke","black")
-    .attr("stroke-width","0.5")   
+    .attr("stroke-width","0.5");
+
+var g = svg.append("g");
+ 
 
 d3.json("iceland_regions.topo.json", function(error, data) {
-	var subunits = topojson.feature(data, data.objects.regions);
-	var projection = d3.geoAlbers()
-    .center([-1.0, 65])
-    .rotate([18, 0])
-    .parallels([50, 60])
-    .scale(7000)
-    .translate([width / 2, height / 2]);
-    var path = d3.geoPath()
-    .projection(projection);
+	var subunits = topojson.feature(data, data.objects.regions);	
   svg.selectAll(".subunit")
     .data(subunits.features)
     .enter().append("path")
@@ -27,15 +33,10 @@ d3.json("iceland_regions.topo.json", function(error, data) {
 	.style("cursor", "pointer")
 	.style("visibility", "Visible")
 	//hér er ég að vinna í því að þegar þú ýttir á einhvern hluta á mapinu þá mun það hidda alla aðra hluta á mapinu
-	.on('mousedown', function(d){	
-		disappear.transition()
-			.style("visibility", "hidden")
-	})		
-	.on('mouseup', function(d){	
-		disappear.transition()	
-			.style("visibility", "Visible")
-
-	})
+	.on('click', function(d){
+		clicked;
+		console.log("test");
+		});	
 });
 	//þetta er ekkert merkilegt sem mun vera endalaust ég mun taka þetta bráðlega
 	function randColor(){
@@ -53,7 +54,7 @@ d3.json("iceland_regions.topo.json", function(error, data) {
 		.center([-1.0, 65])
     	.rotate([18, 0])
     	.parallels([50, 60])
-		.scale(6950)
+		.scale(6950)//lagaði scale fyrir að púnktarnir fara á réttan stað
 
 //lagaði þetta því ég visi hvað var að eftir að sá lat long í öðru json skrá
 d3.csv("capitals.csv", function(error,capitals){
@@ -110,3 +111,39 @@ function wait(ms){
      end = new Date().getTime();
   }
 }
+//þetta átti að geta zoomað inn og út eftir 750 mili sec
+function clicked(d) {
+	 var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  g.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+}
+//virkar ekki
+/*	//þetta á að geta dragað um mapið
+var zoom = d3.zoom()
+  	.on("zoom",function() {
+    	g.attr("transform","translate("+d3.event.translate.join(",")+")scale("+d3.event.scale+")")
+  });
+    
+  svg.call(zoom)	*/
+
+ //þegar ég reyni að nota þetta kemur vill að við d3.event.translate.join villan er .join
+
